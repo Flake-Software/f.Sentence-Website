aec3cee59ad1b86ab43a588ac27d91e5e67b94da
Zoran Jeremić <batajeremiccc@gmail.com>
2026-03-26 20:54:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// Simple interaction for buttons
+document.querySelector('.btn-primary').addEventListener('click', () => {
+    alert('f.Sentence is coming soon!');
+});
 
+console.log("f.Sentence website loaded successfully.");
